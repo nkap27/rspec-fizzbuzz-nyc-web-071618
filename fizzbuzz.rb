ef4a77361ec5e88156a54baeade532a0c5941a65
Natasha K <kapr.natasha@gmail.com>
2018-07-14 20:37:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(num)
-  if num % 3 == 0
+def fizzbuzz(int)
+  if int % 3 == 0
     "Fizz"
-  elsif num % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
-  elsif num % 3 == 0 && num % 5 == 0
+  elsif int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   else
     nil
